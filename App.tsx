7a62b4c32c480d7c9d774708a4b0583c564a2d18
Rajesh <155414306+Rajj25@users.noreply.github.com>
2025-12-06 23:04:40 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { Dashboard } from "./pages/Dashboard";
+import { Dashboard } from "./pages/dashboard";
 import { LivePrices } from "./pages/LivePrices";
 import { News } from "./pages/News";
 import { Signals } from "./pages/Signals";
